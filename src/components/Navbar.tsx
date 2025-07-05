d4188ebddd14df74ce7dc1267a59054f17eebc71
Valentín Gómez <ids41gomezvalen@gmail.com>
2025-07-04 22:34:58 -0300

    ##g

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -44,6 +44,7 @@ export default function Navbar() {
           <Link href="/panel" className={linkClasses('/panel')}>Inicio</Link>
           <Link href="/panel/paciente" className={linkClasses('/panel/paciente')}>Registrar</Link>
           <Link href="/panel/respuestas" className={linkClasses('/panel/respuestas')}>Respuestas</Link>
+          <Link href="/panel/interacciones" className={linkClasses('/panel/interacciones')}>Interacciones</Link>
           {rol === 'superadmin' && (
             <Link href="/panel/clinicas" className={linkClasses('/panel/clinicas')}>Clínicas</Link>
           )}
